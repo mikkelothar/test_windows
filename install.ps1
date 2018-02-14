a765b88e314298d14d7de271854187cee009695f
Mikkel Olsen <mikkelothar@gmail.com>
2018-02-14 13:06:12 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,6 @@
+import-module servermanager
+add-windowsfeature Web-Server, Web-WebServer, Web-Security, Web-Filtering, Web-Windows-Auth, Web-Static-Content, Web-Includes, Web-Mgmt-Console, Web-Custom-Logging
+
 Remove-Item -recurse -Force -Confirm:$false C:\inetpub\wwwroot\*
 cd c:\temp\test_windows
 copy index.html c:\inetpub\wwwroot\
